68970acc318fdd4af4a1af99982074020dac5f6d
sedx876 <swatkins876@gmail.com>
2020-11-07 07:03:38 -0500

    code update

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {AppProvider} from './context'
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AppProvider>
+    <App/>
+  </AppProvider>,
   document.getElementById('root')
 );
 
